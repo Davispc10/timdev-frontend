cdc7d78d64b20f2550cd693e74361ef05716bfe4
Davispc10 <davispc100@gmail.com>
2019-08-16 17:55:18 -0300

    ajuste front

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 import logo from '../assets/logo.svg'
 
 export default function Login () {
+  const [username, setUsername] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    console.log(username)    
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img scr={logo} alt="TimDev" />
-        <input placeholder="Digite seu usuário no GitHub"/>
+        <input placeholder="Digite seu usuário no GitHub" value={username} onChage={e => setUsername(e.target.value)} />
         <button type="submit">Enviar</button>
       </form>
     </div>
